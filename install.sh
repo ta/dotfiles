ad81b513d4e031d642b6e2e08a0df44c786be14e
Tonni Tølbøll Lund Aagesen <tonni@toelboell-lund.dk>
2017-01-01 22:03:30 +0100

    rubocop rule tweaks + disable host key checking in install script

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 # Usage: curl -s https://raw.githubusercontent.com/ta/dotfiles/master/install.sh | sh
 
-echo "Installing dotfiles!"
+echo "Installing dotfiles!\n"
 
 ( cd /tmp && \
+  GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
   git clone -q git@github.com:ta/dotfiles.git && \
   cd dotfiles && \
   rm -rf /tmp/dotfiles/.git /tmp/dotfiles/install.sh && \
@@ -13,4 +14,4 @@ echo "Installing dotfiles!"
 	  rsync -av "`pwd`/$X" "$HOME/$X"
   done && \
   rm -rf /tmp/dotfiles && \
-  echo "Done!" )
+  echo "\nDone!" )
